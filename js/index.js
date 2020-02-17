b1239951e625522cb6426e6c79154f65ac6aa1cd
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-02-16 23:07:40 -0800

    up-date

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Your code goes here
 
+
+// used mouseover for header image
+
 const busInTheSand = document.querySelector('.intro img');
 
 busInTheSand.addEventListener('mouseover',()=>{
@@ -10,3 +13,24 @@ busInTheSand.addEventListener('mouseleave',()=>{
     busInTheSand.src = "img/fun-bus.jpg"
 }
 )
+
+
+// used mouseenter and mouseleave for navigation
+
+const links = document.querySelectorAll('.nav a');
+links.forEach(link=>{
+    link.addEventListener('mouseenter',()=>{
+        link.style.transform = 'scale(1.9)'
+        link.style.color = "#a83232"
+        link.style.transition = "transform 0.7s"
+    })
+})
+
+links.forEach(link=>{
+    link.addEventListener('mouseleave',()=>{
+        link.style.transform = 'scale(1)'
+        link.style.color = "#91a832"
+    })
+})
+
+
