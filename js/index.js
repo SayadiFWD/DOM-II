58f6ac31255cdca54589ba4ac49557f98076a2c5
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-02-16 18:15:57 -0800

    up-date

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const busInTheSand = document.querySelector('.intro img');
+
+busInTheSand.addEventListener('mouseover',()=>{
+    busInTheSand.src = 'https://media.wired.com/photos/5d4dc145b8d30b00081f6eb8/master/pass/Transpo_UTA-541063240.jpg'
+
+})
+busInTheSand.addEventListener('mouseleave',()=>{
+    busInTheSand.src = "img/fun-bus.jpg"
+}
+)
